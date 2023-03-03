941a26e1da8bf582095d7d4d5e07d7a1ed8cd59c
Jermaine Sosa <jibsosa@mymail.mapua.edu.ph>
2023-03-03 15:10:50 +0800

    initial commit

---
+++ SosaBlogDB/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserName] NVARCHAR(16) NOT NULL, 
+    [Firstname] NVARCHAR(50) NOT NULL, 
+    [lastname] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(16) NOT NULL
+)
